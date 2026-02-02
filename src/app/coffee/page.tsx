0b5fdc15cf337c5cde122dc44be327e3b4768d2a
Juan David Bonilla Alvarez <juandavidbonilla2207@gmail.com>
2026-02-02 02:10:39 -0500

    feat: enhance coffee data selection to include review scores

--- src/app/coffee/page.tsx
+++ src/app/coffee/page.tsx
@@ -1,11 +1,17 @@
 import { supabase } from "@/lib/supabase";
 import CoffeeList from "@/components/CoffeeList";
+import { CoffeeWithReview } from "@/types";
 
 export default async function Page() {
 
     const { data: coffees } = await supabase
         .from("coffee")
-        .select("id, name, description, image");
+        .select(`
+            id, 
+            name, 
+            description, 
+            image,
+            review(score)`);
 
-    return <CoffeeList initialCoffees={coffees || []} />;
+    return <CoffeeList initialCoffees={coffees as CoffeeWithReview[] || []} />;
 }
